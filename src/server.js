112d1c654c895c27dd5c6fee590a4d39ee3e8bc1
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2024-06-06 09:56:22 -0300

    03 Rotas de Criação e listagem

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
 import http from 'node:http'
 
+/*
+- HTTP
+  - Método HTTP
+  - URL
+
+GET, POST, PUT, PATCH, DELETE
+*/
+
 const server = http.createServer((request, response) => {
+  const { method, url } = request
+  if (method === 'GET' && url === '/users') {
+    return response.end('Listagem de Usuários')
+  }
+  if (method === 'POST' && url === '/users') {
+    return response.end('Criação de Usuários')
+  }
   return response.end('Hello Ignite!')
 })
 
