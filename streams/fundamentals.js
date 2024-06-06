613ad379031a3580d9a0728578f0ae3cbd6c4846
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2024-06-06 10:26:16 -0300

    07 Criando Streams de leitura

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -6,4 +6,28 @@
 4. POST /upload import.csv
 
 5. Readable Streams / Writeable Streams
+
+process.stdin
+  .pipe(process.stdout)
 */
+
+import { Readable } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+  index = 1
+  
+  _read() {
+    const i = this.index++
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+        return
+      }
+      const buf = Buffer.from(`${i} - `)
+      this.push(buf)
+    }, 1000)
+  }
+}
+
+new OneToHundredStream()
+  .pipe(process.stdout)
