5411838c3f91cebba6845e6044cd8d0cae354f33
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2024-06-06 10:03:31 -0300

    04 Salvando usuários em memória

--- src/server.js
+++ src/server.js
@@ -6,14 +6,27 @@ import http from 'node:http'
   - URL
 
 GET, POST, PUT, PATCH, DELETE
+
+1. Stateful - Stateless
+2. JSON
+3. Cabeçalhos da requisição e resposta
 */
+
+const users = []
 
 const server = http.createServer((request, response) => {
   const { method, url } = request
   if (method === 'GET' && url === '/users') {
-    return response.end('Listagem de Usuários')
+    return response
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users));
   }
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'john.doe@gmail.com'
+    })
     return response.end('Criação de Usuários')
   }
   return response.end('Hello Ignite!')
